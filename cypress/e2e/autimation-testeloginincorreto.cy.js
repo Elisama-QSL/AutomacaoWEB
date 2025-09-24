07851225797be611c3c9f89e2fbf9a5b5770094d
Elisama <elisamaqs@gmail.com>
2025-09-24 18:22:02 -0300

    Validação e Logout

---
+++ cypress/e2e/autimation-testeloginincorreto.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Automation Test Case', () => {
+    it('Login na conta', () => {
+
+        cy.visit('https://automationexercise.com/')
+
+        cy.url().should('include', 'automationexercise')
+
+        cy.get('a[href="/login"]').click()
+        cy.get('.login-form > h2')
+
+        cy.get('[data-qa="login-email"]').type('qa4@gmail.com')
+        cy.get('[data-qa="login-password"]').type('qa', {log: false})
+        cy.get('[data-qa="login-button"]').click()
+
+        cy.get(`.login-form > form > p`)
+    
+        
+    });
+});
